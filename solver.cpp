f3e03fa5bda9f0bde5e49d35ae52f6b3714687fd
Edvard <45853648+EdvardSire@users.noreply.github.com>
2020-07-06 20:52:45 +0200

    Changed the curlybracket styling

--- solver.cpp
+++ solver.cpp
@@ -15,14 +15,19 @@ int grid[9][9] = {
 { 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
 
 // Function that draws the grid
-void drawGrid() {
+void drawGrid()
+{
     cout << "" << endl;
-    for(int i = 0; i < 9; i++) {
-        if((i == 3) || (i == 6)) {
+    for(int i = 0; i < 9; i++) 
+    {
+        if((i == 3) || (i == 6))
+        {
             cout << "---------------------" << endl;
         }
-        for(int j = 0; j < 9; j++) {
-            if((j == 3) || (j == 6)) {
+        for(int j = 0; j < 9; j++)
+        {
+            if((j == 3) || (j == 6))
+            {
                 cout << "| ";
             }
             cout << grid[i][j];
@@ -34,9 +39,12 @@ void drawGrid() {
 
 // Returns bool based on if 'test' is in 'row'
 // Returns 0 if value is not in row (horiozontal segment), returns 1 if value is in row
-bool isInRow(int row, int test) {
-    for(int i = 0; i < 9; i++) {
-        if(grid[row][i] == test) {
+bool isInRow(int row, int test)
+{
+    for(int i = 0; i < 9; i++)
+    {
+        if(grid[row][i] == test)
+        {
             return true;
         }
     }
@@ -45,9 +53,12 @@ bool isInRow(int row, int test) {
 
 // Returns bool based on if 'test' is in 'col'
 // Returns 0 if value is not in col (vertical segment), returns 1 if value is in col
-bool isInCol(int col, int test) {
-    for(int i = 0; i < 9; i++) {
-        if(grid[i][col] == test) {
+bool isInCol(int col, int test)
+{
+    for(int i = 0; i < 9; i++)
+    {
+        if(grid[i][col] == test)
+        {
             return true;
         }
     }
@@ -56,12 +67,16 @@ bool isInCol(int col, int test) {
 
 // Returns bool bases on if 'test' is in the box based on intput row and col
 // Returns 0 if value is not in box (3x3 square segment), returns 1 if value is in box
-bool isInBox(int row, int col, int test) {
+bool isInBox(int row, int col, int test)
+{
     int boxRow = row-row%3;
     int boxCol = col-col%3;
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(grid[boxRow + i][boxCol + j] == test) {
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(grid[boxRow + i][boxCol + j] == test)
+            {
                 return true;
             }
         }
@@ -71,16 +86,21 @@ bool isInBox(int row, int col, int test) {
 
 // Function that does the recursion
 // Returns 1 if there are no spaces to fill
-bool solve() {
+bool solve()
+{
     bool gotValue = false;
     int row = 0; int col = 0; int outOfBoundsCheck = 0;
 
     // Finds zero
-    while(!gotValue && (outOfBoundsCheck < 81)) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
+    while(!gotValue && (outOfBoundsCheck < 81))
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
                 outOfBoundsCheck++;
-                if(grid[i][j] == 0) {
+                if(grid[i][j] == 0)
+                {
                     row = i; col = j;
                     gotValue = true;
                     goto endGotValueLoop;
@@ -93,11 +113,15 @@ bool solve() {
     }
 
     // If no zero was found return true, else try nums from 1-9 and call solve()
-    if(!gotValue) {
+    if(!gotValue)
+    {
         return true;
-    } else {
-        for(int i = 1; i < 10; i++) {
-            if(!isInRow(row, i) && !isInCol(col, i) && !isInBox(row, col, i)) {
+    } else
+    {
+        for(int i = 1; i < 10; i++)
+        {
+            if(!isInRow(row, i) && !isInCol(col, i) && !isInBox(row, col, i))
+            {
                 grid[row][col] = i;
                 if(solve() == true)
                     return true;
@@ -109,7 +133,8 @@ bool solve() {
     }
 }
 
-int main() {
+int main()
+{
     solve();
     drawGrid();
 }
